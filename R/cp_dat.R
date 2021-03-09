fe9f607754ba8d9daf6886e96955654922d73421
Kyle Humphrey <khumph2@gmail.com>
2021-03-09 11:32:39 -0800

    Read a single sas file; move cp_dat

---
+++ R/cp_dat.R
@@ -0,0 +1,30 @@
+#' Copy all of the files in a directory into another directory
+#'
+#' Copies files from one directory into another, only copying files that have been updated.
+#' @param dir_in The directory with files to copy
+#' @param dir_out The directory to copy into. If this doesn't exist it will be created.
+#' @param ... Additional arugments passed to fs::dir_ls()
+#' @export
+cp_dat <- function(dir_in, dir_out, ...) {
+  paths_there <- fs::dir_ls(dir_in, ...)
+
+  fs::dir_create(dir_out, recurse = TRUE)
+  paths_here <- fs::dir_ls(dir_out, ...)
+
+  files_there_names <- fs::path_file(paths_there)
+  files_here_names <- fs::path_file(paths_here)
+
+  files_there_here <- files_here_names %in% files_there_names
+  files_here_there <- files_there_names %in% files_here_names
+
+  files_there_info <- fs::file_info(paths_there[files_here_there])
+  files_here_info <- fs::file_info(paths_here[files_there_here])
+
+  newer_there <- files_there_info$modification_time > files_here_info$modification_time
+
+  paths_newer_there <- paths_there[files_here_there][newer_there]
+  paths_not_here <- paths_there[!files_here_there]
+  paths_to_copy <- c(paths_newer_there, paths_not_here)
+
+  fs::file_copy(paths_to_copy, dir_out, overwrite = TRUE)
+}
--- R/read_sas_files.R
+++ R/read_sas_files.R
@@ -1,47 +1,30 @@
-#' Read in all of the SAS files in a directory
+#' Read in a single SAS file, converting names to lowercase and removing labels
 #'
-#' @param directory The directory containing \code{.sas7bdat} files.
+#' @param path The path to a \code{.sas7bdat} file.
+#' @param var_labs Retain the SAS variable labels?
 #' @export
-read_sas_files <- function(directory) {
-  filenames <- list.files(directory, pattern = '\\.sas7bdat$')
-  paths <- file.path(directory, filenames)
-  out <- lapply(paths, function(x) {
-    dat <- haven::read_sas(x)
-    names(dat) <- tolower(names(dat))
+read_sas_file <- function(path, var_labs = FALSE) {
+  dat <- haven::read_sas(path)
+  names(dat) <- tolower(names(dat))
+  if (var_labs) {
     return(dat)
-  })
-  names(out) <- tools::file_path_sans_ext(filenames)
-  return(out)
+  }
+  for (name in names(dat)) {
+    attr(dat[[name]], "label") <- NULL
+  }
+  return(dat)
 }
 
 
-#' Copy all of the files in a directory into another directory
+#' Read in all of the SAS files in a directory
 #'
-#' Copies files from one directory into another, only copying files that have been updated.
-#' @param dir_in The directory with files to copy
-#' @param dir_out The directory to copy into. If this doesn't exist it will be created.
-#' @param ... Additional arugments passed to fs::dir_ls()
+#' @param directory The directory containing \code{.sas7bdat} files.
+#' @param var_labs Retain the SAS variable labels?
 #' @export
-cp_dat <- function(dir_in, dir_out, ...) {
-  paths_there <- fs::dir_ls(dir_in, ...)
-
-  fs::dir_create(dir_out, recurse = TRUE)
-  paths_here <- fs::dir_ls(dir_out, ...)
-
-  files_there_names <- fs::path_file(paths_there)
-  files_here_names <- fs::path_file(paths_here)
-
-  files_there_here <- files_here_names %in% files_there_names
-  files_here_there <- files_there_names %in% files_here_names
-
-  files_there_info <- fs::file_info(paths_there[files_here_there])
-  files_here_info <- fs::file_info(paths_here[files_there_here])
-
-  newer_there <- files_there_info$modification_time > files_here_info$modification_time
-
-  paths_newer_there <- paths_there[files_here_there][newer_there]
-  paths_not_here <- paths_there[!files_here_there]
-  paths_to_copy <- c(paths_newer_there, paths_not_here)
-
-  fs::file_copy(paths_to_copy, dir_out, overwrite = TRUE)
+read_sas_files <- function(directory, var_labs = FALSE) {
+  filenames <- list.files(directory, pattern = '\\.sas7bdat$')
+  paths <- file.path(directory, filenames)
+  out <- lapply(paths, read_sas_file, var_labs)
+  names(out) <- tools::file_path_sans_ext(filenames)
+  return(out)
 }
